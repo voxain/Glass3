76f63be71e6d3ef1b1a9915900be98d5f1fdb26b
Rene <info@rmcprod.me>
2023-08-01 17:10:07 +0200

    :heavy_plus_sign: The solution was using a better library.
    Yes, I stole the code from their demos page.

--- src/System/Libraries/WindowManager.js
+++ src/System/Libraries/WindowManager.js
@@ -84,10 +84,75 @@ export class Window
 
         // TODO: Fetch app's desired window HTML and apply to template
         this.#Window = GWM.Desktop.appendChild(this.#Window.body.childNodes[0]);
-        let movable = new Moveable(GWM.Desktop, {target: this.#Window, draggable: true, hideDefaultLines: true}).on("drag", () => {
-            console.log("DRAG!!")
-        });// ! TODO: Figure out why window is not draggable but event does fire
-        console.log(movable)
+
+        interact(this.#Window).resizable({
+            // resize from all edges and corners
+            edges: { left: true, right: true, bottom: true, top: true },
+        
+            listeners: {
+              move (event) {
+                var target = event.target
+                var x = (parseFloat(target.getAttribute('data-x')) || 0)
+                var y = (parseFloat(target.getAttribute('data-y')) || 0)
+        
+                // update the element's style
+                target.style.width = event.rect.width + 'px'
+                target.style.height = event.rect.height + 'px'
+        
+                // translate when resizing from top or left edges
+                x += event.deltaRect.left
+                y += event.deltaRect.top
+        
+                target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+        
+                target.setAttribute('data-x', x)
+                target.setAttribute('data-y', y)
+              }
+            },
+            modifiers: [
+              // keep the edges inside the parent
+              interact.modifiers.restrictEdges({
+                outer: 'parent'
+              }),
+        
+              // minimum size
+              interact.modifiers.restrictSize({
+                min: { width: 200, height: 100 }
+              })
+            ],
+        
+            inertia: true
+          }).draggable({
+            allowFrom: '.titlebar',
+            // enable inertial throwing
+            inertia: true,
+            // keep the element within the area of it's parent
+            modifiers: [
+              interact.modifiers.restrictRect({
+                restriction: 'parent',
+                endOnly: true
+              })
+            ],
+            // enable autoScroll
+            autoScroll: true,
+        
+            listeners: {
+              // call this function on every dragmove event
+              move: dragMoveListener,
+        
+              // call this function on every dragend event
+              end (event) {
+                var textEl = event.target.querySelector('p')
+        
+                textEl && (textEl.textContent =
+                  'moved a distance of ' +
+                  (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                             Math.pow(event.pageY - event.y0, 2) | 0))
+                    .toFixed(2) + 'px')
+              }
+            }
+          })
+          // TODO: Clean up stolen code from demo page.
     }
 
     SetTitle(title){
@@ -110,3 +175,17 @@ export class Window
 
     // TODO: Add event emitters for when something is updated, so Docks/other apps can react to title changes etc...
 }
+
+function dragMoveListener (event) {
+    var target = event.target
+    // keep the dragged position in the data-x/data-y attributes
+    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+  
+    // translate the element
+    target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+  
+    // update the posiion attributes
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
+  }
